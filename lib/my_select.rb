2a5bddbf3dc728a5af38a8fe379edb8389643c56
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-16 17:27:41 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ new_array = []
+ while i < collection.length
+   elem = yield (collection[i])
+   if elem == true
+     new_array << collection[i]
+   end
+   i += 1
+ end
+ new_array
 end
