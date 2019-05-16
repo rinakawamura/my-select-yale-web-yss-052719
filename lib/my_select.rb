def0fb3f34052a2c3550931c05e86c0f8b700d41
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-16 17:27:49 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0
  new_array = []
  while i < collection.length
-   elem = yield (collection[i])
+   elem = yield collection[i]
    if elem
      new_array << collection[i]
    end
